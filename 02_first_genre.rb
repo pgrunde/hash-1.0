5d911518f84f7b09a39bbebe4f4be7054501dee8
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-19 14:39:14 -0600

    added genre and neighbor

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,5 +1,9 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
-p PEOPLE["joe"][:"preferences"][:"favorite_genres"][0]
-p PEOPLE["sue"][:"preferences"][:"favorite_genres"][0]
+# p PEOPLE["joe"][:preferences][:favorite_genres][0]
+# p PEOPLE["sue"][:preferences][:favorite_genres][0]
+
+PEOPLE.each_key do |k|
+  p PEOPLE[k][:preferences][:favorite_genres].first
+end
--- 03_joes_neighbor.rb
+++ 03_joes_neighbor.rb
@@ -3,3 +3,5 @@ require_relative 'people'
 # What is the last name of the person sitting next to joe?
 #
 # HINT: Start by finding who joe is sitting next to, then find that person in the hash.
+next_to = PEOPLE["joe"][:sitting_next_to].to_s
+p PEOPLE[next_to][:last_name]
