2b5360525a125a4ed1b0ecbea6fe66c190bb8658
Peter Grunde <peter.a.grunde@gmail.com>
2014-06-19 14:17:02 -0600

    added first genres

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,5 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+p PEOPLE["joe"][:"preferences"][:"favorite_genres"][0]
+p PEOPLE["sue"][:"preferences"][:"favorite_genres"][0]
